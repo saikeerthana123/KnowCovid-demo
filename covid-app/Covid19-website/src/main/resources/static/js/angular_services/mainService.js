e71d5f589c537b9a4173deb96126f0c7dcbdb066
Sai Keerthana Goruganthu <67637168+saikeerthana123@users.noreply.github.com>
2023-04-09 16:29:37 -0500

    Update mainService.js

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -2,7 +2,7 @@ app.service('mainService', function($http, $q, $location, $window) {
 
 	//local
     //var serverURL = "http://localhost:9300/Covid-19/";
-    var serverURL = "http://Server_IP:9090/Covid-19/";
+    var serverURL = "http://3.215.117.226:9090/Covid-19/";
     
 	return {
 	   callPostRestAPI : function(url, data) {
